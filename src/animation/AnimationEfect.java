7ec69276aa8b8f27ad2b7f2279c560cd23cdb65b
bitPanG98 <32599376+bitPanG98@users.noreply.github.com>
2020-04-13 17:14:48 -0500

    Add new functions

--- src/animation/AnimationEfect.java
+++ src/animation/AnimationEfect.java
@@ -12,20 +12,6 @@
  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-    
- Redistribution and use in source and binary forms, with or without modification,
- are permitted provided that the following conditions are met:
-
- 1. Redistributions of source code must retain the above copyright notice,
- this list of conditions and the following disclaimer.
-
- 2. Redistributions in binary form must reproduce the above copyright notice,
- this list of conditions and the following disclaimer in the documentation
- and/or other materials provided with the distribution.
-
- 3. Neither the name of the copyright holder nor the names of its contributors
- may be used to endorse or promote products derived from this software without
- specific prior written permission. 
 
  */
 package animation;
@@ -37,7 +23,12 @@ import java.awt.Container;
 import java.awt.Shape;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.geom.Arc2D;
+import static java.awt.geom.Arc2D.OPEN;
+import static java.awt.geom.Arc2D.PIE;
 import java.awt.geom.RoundRectangle2D;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPasswordField;
@@ -53,14 +44,70 @@ import util.Util;
  */
 public class AnimationEfect {
 
-    public static int ACTION_DO_NOTHING = 0;
-    public static int ACTION_EXIT = 1;
-    public static int ACTION_DISPOSE = 2;
+    //Window action after animation
+    public static int ACTION_EXIT = 0;
+    public static int ACTION_DISPOSE = 1;
+    public static int ACTION_DO_NOTHING = 2;
 
     private static boolean typeContainerAllowed = false;
     private static TypeContainer typeContainer = null;
 
-    public static void toFadeWindow(final int opacityStartLevel, final int opacityEndLevel, final long speed, Container miContainer, final int actionAfter) {
+    public static void FadeOut(final long speed, Container miContainer, final int actionAfter) {
+
+        if (!Util.isTranslucencySupported()) {
+            System.err.println("[!]> Atencion: Translucency is not supported in this environment. ");
+            System.exit(0);
+        } else {
+
+            typeContainerAllowed = Util.isContainerAllowed(miContainer);
+            if (!typeContainerAllowed) {
+                System.err.println("[!]> Atention: Container type not allowed. ");
+            } else {
+
+                typeContainer = new TypeContainer();
+                typeContainer.setContainer(miContainer);
+
+                if (!Util.isWindowUndecorate(typeContainer)) {
+                    System.err.println("[!]>Atention:  The window is decorated, disable this property.");
+                } else {
+
+                    Thread thread = new Thread(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            float opacity = 0.01f;
+
+                            for (int i = 100; i >= 0; i--) {
+
+                                try {
+
+                                    Thread.sleep(speed);
+
+                                    opacity = i * 0.01f;
+                                    setWindowOpacity(opacity);
+
+                                } catch (InterruptedException ex) {
+                                    System.err.println("[!]> the thread is interrupted, either before or during the activity. \n" + ex.getMessage());
+                                }
+
+                            }
+                            setActionAfter(actionAfter);
+
+                        }
+
+                    });
+                    thread.start();
+
+                }
+
+            }
+
+        }
+
+    }
+
+    public static void FadeOut(final int opacityStartLevel, final int opacityEndLevel, final long speed, Container miContainer, final int actionAfter) {
 
         if (!Util.isTranslucencySupported()) {
             System.err.println("[!]> Atencion: Translucency is not supported in this environment. ");
@@ -115,7 +162,64 @@ public class AnimationEfect {
 
     }
 
-    public static void toShowWindow(final int opacityStartLevel, final int opacityEndLevel, final long speed, Container miContainer, final int actionAfter) {
+    public static void FadeIn(final long speed, Container miContainer, final int actionAfter) {
+
+        typeContainer = new TypeContainer();
+
+        if (!Util.isTranslucencySupported()) {
+            System.err.println("[!]> Atencion: Translucency is not supported in this environment. ");
+            System.exit(0);
+        } else {
+
+            typeContainerAllowed = Util.isContainerAllowed(miContainer);
+            if (!typeContainerAllowed) {
+                System.err.println("[!]> Atention: Container type not allowed. ");
+            } else {
+
+                typeContainer = new TypeContainer();
+                typeContainer.setContainer(miContainer);
+
+                if (!Util.isWindowUndecorate(typeContainer)) {
+                    System.err.println("[!]> Atention: The window is decorated, disable this property.");
+                } else {
+
+                    Thread thread = new Thread(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            float opacity = 0.01f;
+
+                            for (int i = 100; i <= 0; i++) {
+
+                                try {
+
+                                    Thread.sleep(speed);
+
+                                    opacity = i * 0.01f;
+                                    setWindowOpacity(opacity);
+
+                                } catch (InterruptedException ex) {
+                                    System.out.println("[!]> Atention:  the thread is interrupted, either before or during the activity. \n" + ex.getMessage());
+                                }
+
+                            }
+                            setActionAfter(actionAfter);
+
+                        }
+
+                    });
+                    thread.start();
+
+                }
+
+            }
+
+        }
+
+    }
+
+    public static void FadeIn(final int opacityStartLevel, final int opacityEndLevel, final long speed, Container miContainer, final int actionAfter) {
 
         typeContainer = new TypeContainer();
 
@@ -186,21 +290,76 @@ public class AnimationEfect {
 
     private static void setActionAfter(int actionAfter) {
 
-        if (actionAfter == ACTION_DO_NOTHING) {
-            //
-        } else if (actionAfter == ACTION_DISPOSE) {
+        if (actionAfter == ACTION_DISPOSE) {
 
             if (typeContainer.getMiFrame() != null) {
                 typeContainer.getMiFrame().dispose();
-                typeContainer.getMiFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                typeContainer.getMiDialog().setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            }
-            if (typeContainer.getMiDialog() != null) {
+            } else if (typeContainer.getMiDialog() != null) {
                 typeContainer.getMiDialog().setVisible(false);
+            } else {
+                //pass
             }
 
         } else if (actionAfter == ACTION_EXIT) {
             System.exit(0);
+        } else {
+            //pass
+        }
+
+    }
+
+    /*
+     @param type The integer constant that represents the closure
+     */
+    public void setArcType(int type) {
+        if (type < OPEN || type > PIE) {
+            throw new IllegalArgumentException("invalid type for Arc: " + type);
+        }
+    }
+
+    public static void Arc2D(final long speed, final Container miContainer, final int actionAfter) {
+
+        typeContainerAllowed = Util.isContainerAllowed(miContainer);
+        if (!typeContainerAllowed) {
+            System.err.println("[!]> Atention: Container type not allowed. ");
+        } else {
+
+            typeContainer = new TypeContainer();
+            typeContainer.setContainer(miContainer);
+
+            if (!Util.isWindowUndecorate(typeContainer)) {
+                System.err.println("[!]>Atention:  The window is decorated, disable this property.");
+            } else {
+
+                Thread hilo = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        for (int i = 0; i <= 360; i += 20) {
+                            try {
+                                Thread.sleep(300);
+
+                                Arc2D a2D = new Arc2D.Double(0, 0, miContainer.getWidth(), miContainer.getHeight(), 0, i, Arc2D.OPEN);
+                                if (typeContainer.getMiFrame() != null) {
+                                    typeContainer.getMiFrame().setShape(a2D);
+                                } else if (typeContainer.getMiDialog() != null) {
+                                    typeContainer.getMiDialog().setShape(a2D);
+                                } else {
+
+                                }
+
+                            } catch (InterruptedException ex) {
+                                System.err.println("[!]> the thread is interrupted, either before or during the activity. \n" + ex.getMessage());
+                            }
+
+                        }
+                        setActionAfter(actionAfter);
+                    }
+                });
+                hilo.start();
+
+            }
         }
 
     }
@@ -218,18 +377,17 @@ public class AnimationEfect {
     }
 
     /**
-     * Permite que los componentes(JTexfield o JPasswordField) que se encuentran
-     * en un Container(JPanel), cambien su Background y Border si estan vacios o
-     * no.
+     * TexBoxes that are in a container, change their background and border if
+     * they are empty.
      *
-     * @param container es el contenedor de componentes(JPanel).
+     * @param container -> TextBox component container.
      *
      */
     public static void paintTextBoxEmpty(final Container container) {
 
-        for (Component component : container.getComponents()) {
+        for (Component miComponent : container.getComponents()) {
 
-            component.addFocusListener(new FocusListener() {
+            miComponent.addFocusListener(new FocusListener() {
 
                 @Override
                 public void focusGained(FocusEvent e) {
@@ -244,15 +402,13 @@ public class AnimationEfect {
                         if (component instanceof JTextField
                                 || component instanceof JPasswordField) {
 
-                            String valueInput = ((JTextField) component).getText().trim().replace(" ", "");
-                            if (valueInput.equals("")) {
-                                ((JTextField) component).
-                                        setBackground(new Color(255, 153, 153));
+                            String valueTextBox = ((JTextField) component).getText().trim().replace(" ", "");
+                            if (valueTextBox.equals("")) {
+                                ((JTextField) component).setBackground(new Color(255, 153, 153));
                                 ((JTextField) component).
                                         setBorder(BorderFactory.createLineBorder(Color.RED, 1, true));
                             } else {
-                                ((JTextField) component).
-                                        setBackground(Color.WHITE);
+                                ((JTextField) component).setBackground(Color.WHITE);
                                 ((JTextField) component).
                                         setBorder(BorderFactory.createLineBorder(
                                                         new java.awt.Color(0, 204, 204), 1, true));
